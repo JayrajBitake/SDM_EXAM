3d6800e4fe52e3feeef4a21a19d3973a51c896a8
Jayraj <jayrajbitake1@gmail.com>
2023-02-02 14:39:29 +0530

    First Commit

---
+++ LinkList.java
@@ -0,0 +1,20 @@
+import java.util*;
+
+public class LinkList {
+
+    public static void main(String args[]) {
+
+        LinkedList<String> ll = new LinkedList<String>();
+
+        // Adding elements to the linked list
+        ll.add("A");
+        ll.add("B");
+        ll.addLast("C");
+        ll.addFirst("D");
+        ll.add(2, "E");
+
+        System.out.println(ll);
+
+    }
+
+}
